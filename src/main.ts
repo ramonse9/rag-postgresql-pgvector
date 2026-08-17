5d94afd7cb7e058648395ea37654ea7120fb1d49
Ramon Guzman <ramonantoniojr@gmail.com>
2026-08-17 10:09:58 -0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://rag-resume.onrender.com'],
+    origin: ['http://localhost:5173', 'https://rag-front-jizp.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
